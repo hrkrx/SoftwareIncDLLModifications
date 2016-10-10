68617b45cc7557d9a39318ea1d8ef2d513965ee2
seeed <seeed@DESKTOP-0M5L2SC>
2016-10-11 01:24:28 +0200

    More Multiplayer preperations

---
+++ MultiplayerServer/Command.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerServer
+{
+    public class Command
+    {
+        public CommandType commandType;
+        public string target;
+        public string source;
+        public string message;
+        public string serializedCompany;
+
+    }
+
+    public enum CommandType
+    {
+        Login,
+        Logout,
+        Hack,
+        Blame,
+        Update
+    }
+
+    public enum HackType
+    {
+        Computer,
+        Server,
+        Progress,
+        Capital
+    }
+}
---
+++ MultiplayerServer/ConnectionController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace MultiplayerServer
+{
+    public class ConnectionController
+    {
+        public delegate void NewConnectionEvent();
+        public NewConnectionEvent newConnectionEventHandler;
+        public ConcurrentQueue<Command> commands;
+
+        public ConnectionController(NewConnectionEvent eventHandler)
+        {
+            newConnectionEventHandler = eventHandler;
+            commands = new ConcurrentQueue<Command>();
+        }
+
+        public void StartListening()
+        {
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint endpoint = new IPEndPoint(ip, 9999);
+            TcpListener listener = new TcpListener(endpoint);
+            
+            while(true)
+            {
+                var newClient = listener.AcceptTcpClient();
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ConnectionHandler), newClient);
+            }
+
+        }
+
+        public void ConnectionHandler(object tcp)
+        {
+            var client = (TcpClient)tcp;
+
+
+
+        }
+    }
+}
