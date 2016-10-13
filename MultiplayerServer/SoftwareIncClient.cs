61f0efe8f2fafb4d82c3a941932303fedf954017
hrkrx <hrkrx@users.noreply.github.com>
2016-10-13 02:22:12 +0200

    testcommit

--- MultiplayerServer/SoftwareIncClient.cs
+++ MultiplayerServer/SoftwareIncClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,6 @@ namespace MultiplayerServer
 {
     public class SoftwareIncClient
     {
+        TcpClient client;
     }
 }
